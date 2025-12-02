0dd273e97b0299708f7a8ed700ab3b6afa9c1e21
YkaRUs Rodriguez <axel.rodriguez@laplateforme.com>
2025-12-02 11:21:03 +0100

    Solo gamemode added

--- gamemode.py
+++ gamemode.py
@@ -1,3 +1,63 @@
+from random import randint
+
+def computer(board, sign):
+    while True:
+        try:
+            ai_choice = randint(1, 9)
+
+            if str(ai_choice) not in board:
+                raise ValueError
+            
+            else:
+                board[ai_choice - 1] = sign
+                break
+
+        except ValueError:
+            print("Entrez un nombre valide ")
+    return sign
+
+def gamemode1player(board, win, players, ai_sign):
+
+    if players == ai_sign:
+        
+        computer(board, ai_sign)
+
+        for combo in win:
+            if all(board[ai_sign] == players for ai_sign in combo):
+                return combo
+        return None
+    
+    else:
+
+        print("\n", " | ".join(board[:3]))
+        print("---+---+---")
+        print("", " | ".join(board[3:6]))
+        print("---+---+---")
+        print("", " | ".join(board[6:]))
+
+        while True:
+            try:
+                choice = input(f"{players}, Choisissez un numéro : ")
+
+                if choice not in board:
+                    raise ValueError
+                
+                else:
+                    board[int(choice) - 1] = players
+                    break
+
+            except ValueError:
+                print("Entrez un nombre valide ")
+
+        # Win condition
+        for combo in win:
+            if all(board[choice] == players for choice in combo):
+                return combo
+            
+        # No return if no player win 
+        return None
+
+
 def gamemode2player(board, win, players):
 
     # Print the grid    
@@ -10,11 +70,13 @@ def gamemode2player(board, win, players):
     # Game loop 
     while True:
         try:
-            choice = int(input(f"{players}, Choisissez un numéro : "))
-            if str(choice) not in board:
+            choice = input(f"{players}, Choisissez un numéro : ")
+
+            if choice not in board:
                 raise ValueError
+            
             else:
-                board[choice-1] = players
+                board[int(choice) - 1] = players
                 break
 
         except ValueError:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Import functions from other files
 
-from gamemode import gamemode2player
+from gamemode import gamemode2player, gamemode1player
 
 # Listing each combos that lead to winning
 
@@ -18,17 +18,53 @@ player1 = "X"
 player2 = "O"
 player = player1
 
-# Game loop until winner
-for i in range(9):
-    win = gamemode2player(grid, combos, player)
-    if win:
-        print(f"Le joueur '{player}' a gagné !")
-        break
+gamemode_choice = input("1 - Mode solo\n2 - Mode 2 joueurs")
 
-    player = player1 if player == player2 else player2
+if gamemode_choice == "1":
+        # Player creation
+    player1 = "X"
+    ai = "O"
+    player = player1
+
+    # Game loop until winner
+    for i in range(9):
+
+        win = gamemode1player(grid, combos, player, ai)
+
+        if win:       
+            print(f"Le joueur '{player}' a gagné !")
+            break
+
+        if player == player1:
+            player = ai
+        
+        else:
+            player = player1
+    else:
+        # 9 moves without a win is a draw
+        print("Il y a eu égalité.")
+
+elif gamemode_choice == "2":
+    # Player creation
+    player1 = "X"
+    player2 = "O"
+    player = player1
+
+    # Game loop until winner
+    for i in range(9):
+
+        win = gamemode2player(grid, combos, player)
+        
+        if win:       
+            print(f"Le joueur '{player}' a gagné !")
+            break
+
+        player = player1 if player == player2 else player2
+
+    else:
+
+        # 9 moves without a win is a draw
+        print("Il y a eu égalité.")
 
 else:
-
-    # 9 moves without a win is a draw
-    print("Il y a eu égalité.")
-
+    print("Erreur du programme, essayez d'entrer un nombre correct.")
