79a593a9783d4b482a687fafe38e34ad8bdcdd93
YkaRUs Rodriguez <axel.rodriguez@laplateforme.com>
2025-12-03 11:35:33 +0100

    Deleted the over commentary sections

--- gamemode.py
+++ gamemode.py
@@ -2,21 +2,16 @@ from random import randint
 import time
 
 def restart_choice():
-    # Loop to handle an error
     while True:
-            # Asking the player
             restart_choice = input("Voulez-vous rejouer ? (O/N)\n").lower()
 
-            # Breaking the actual while true, leading to the game while true
             if restart_choice == "o":
                 break
             
-            # quit the program even if there's more than one loop
             elif restart_choice == "n":
                 print("Merci d'avoir joué !")
                 return quit()
 
-            # Error -> Program loop on the choice
             else:
                 print("Erreur, veuillez réessayer")
                 time.sleep(0.5)
@@ -24,45 +19,38 @@ def restart_choice():
     return None
 
 def computer(board, sign):
-    # Loop to handle errors
+
     while True:
-        # Algo choosing a random int
         ai_choice = randint(1, 9)
 
-        # Checking if ai is choosing a valid number
         if str(ai_choice) not in board:
             continue
         
-        # Changing the number in the board by the sign of the ai
         else:
             board[ai_choice - 1] = sign
             break
-    # Return the changes
+
     return sign
 
 def gamemode1player(board, win, players, ai_sign):
 
-    # Checking if the player of the turn is the ai or not
     if players == ai_sign:
-        # If so, it lead to the computer function
         computer(board, ai_sign)
-        # Checking after he choose if he won or not
+
         for combo in win:
             if all(board[ai_sign] == players for ai_sign in combo):
                 return combo
+            
         return None
     
-    # re-using the function to handle player turn
     else:
 
-        # Printing the board
         print("\n", " | ".join(board[:3]))
         print("---+---+---")
         print("", " | ".join(board[3:6]))
         print("---+---+---")
         print("", " | ".join(board[6:]))
 
-        # Game loop (same as the player loop in the 1 player gamemode)
         while True:
             try:
                 choice = input(f"{players}, Choisissez un numéro : ")
@@ -77,26 +65,22 @@ def gamemode1player(board, win, players, ai_sign):
             except ValueError:
                 print("Entrez un nombre valide ")
 
-        # Win condition
         for combo in win:
             if all(board[choice] == players for choice in combo):
                 return combo
             
-        # No return if no player win 
         return None
 
 
 
 def gamemode2player(board, win, players):
 
-        # Printing the board
     print("\n", " | ".join(board[:3]))
     print("---+---+---")
     print("", " | ".join(board[3:6]))
     print("---+---+---")
     print("", " | ".join(board[6:]))
 
-    # Game loop (same as the player loop in the 1 player gamemode)
     while True:
         try:
             choice = input(f"{players}, Choisissez un numéro : ")
@@ -111,10 +95,8 @@ def gamemode2player(board, win, players):
         except ValueError:
             print("Entrez un nombre valide ")
 
-    # Win condition
     for combo in win:
         if all(board[choice] == players for choice in combo):
             return combo
         
-    # No return if no player win 
     return None
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# All import needed
 import time
 from gamemode import gamemode2player, gamemode1player, restart_choice
-
-# Listing each combos that lead to winning
 
 combos = (
     (0, 1, 2), (3, 4, 5), (6, 7, 8),
@@ -12,7 +9,6 @@ combos = (
 
 while True:
 
-    # List that appear in each gamemode before a sign is applied 
     grid = ["1","2","3","4","5","6","7","8","9"]
 
     gamemode_choice = input("1 - Mode solo\n2 - Mode 2 joueurs\n")
@@ -20,12 +16,10 @@ while True:
     # Solo mode
     if gamemode_choice == "1":
 
-        # Player creation
         player1 = "X"
         ai = "O"
         player = player1
 
-        # Game loop until winner
         for i in range(9):
 
             win = gamemode1player(grid, combos, player, ai)
@@ -39,22 +33,19 @@ while True:
             
             else:
                 player = player1
-        # 9 moves without a win is a draw
+
         else:
             print("Il y a eu égalité.")
 
-        # Asking player to replay or not
         restart_choice()
 
     # 2 players mode
     elif gamemode_choice == "2":
 
-        # Player creation
         player1 = "X"
         player2 = "O"
         player = player1
 
-        # Game loop until winner
         for i in range(9):
 
             win = gamemode2player(grid, combos, player)
@@ -65,12 +56,10 @@ while True:
 
             player = player1 if player == player2 else player2
 
-        # 9 moves without a win is a draw
         else:
 
             print("Il y a eu égalité.")
 
-        # Asking player to replay or not
         restart_choice()
 
     # Error input for the choice of gamemode
